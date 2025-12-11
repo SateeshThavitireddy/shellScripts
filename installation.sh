4f79a1f0baff2bc63354cd86bbe6514909f91a46
sateesh thavitireddy <sateesh.thavitireddy@gmail.com>
2025-12-11 07:52:31 +0000

    adding the code

--- installation.sh
+++ installation.sh
@@ -6,7 +6,7 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-if [$USERID -ne 0 ];then
+if [$USERID -ne 0 ]; then
     echo "ERROR:: Please run this script with root privileges."
     exit 1
 fi 
@@ -19,23 +19,22 @@ fi
 #    echo -e "Mysql already installed.... $G Skipping the installation $N"
 #fi
 
-CHECK_ALREADY_INSTALLED $? "nginx"
-
 CHECK_ALREADY_INSTALLED(){
     if [ $1 -ne 0 ]: then
         echo -e "$Y $2 is not installed on the system. Proceeding with installation... $N"
         dnf install nginx -y
         VALIDATE $? "nginx"
     else 
-    echo -e "$2 already installed.... $G Skipping the installation $N"
+        echo -e "$2 already installed.... $G Skipping the installation $N"
     fi
 }
 
 VALIDATE(){
-     if [$1 -ne 0 ]; then
-    echo "ERROR:: Installinig $2 Failed"
-    exit 1
-else 
-    echo -e " $2  is Installed Successfully"
-fi
+    if [$1 -ne 0 ]; then
+        echo "ERROR:: Installinig $2 Failed"
+        exit 1
+    else 
+        echo -e " $2  is Installed Successfully"
+    fi
 }
+CHECK_ALREADY_INSTALLED $? "nginx"
