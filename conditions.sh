eb0d254ff3eae0d5e6c45055e2117286f0b15aa9
sateesh thavitireddy <sateesh.thavitireddy@gmail.com>
2025-12-11 07:30:33 +0000

    adding the code

--- conditions.sh
+++ conditions.sh
@@ -1,8 +1,56 @@
 #!/bin/bash
 
 NUMBER=$1
+
+# LESS THAN (-lt)
+echo "=== LESS THAN (-lt) ==="
 if [ $NUMBER -lt 10 ]; then
     echo "The number $NUMBER is less than 10"
 else 
+    echo "The number $NUMBER is NOT less than 10"
+fi
+
+# GREATER THAN (-gt)
+echo ""
+echo "=== GREATER THAN (-gt) ==="
+if [ $NUMBER -gt 10 ]; then
+    echo "The number $NUMBER is greater than 10"
+else
+    echo "The number $NUMBER is NOT greater than 10"
+fi
+
+# LESS THAN OR EQUAL (-le)
+echo ""
+echo "=== LESS THAN OR EQUAL (-le) ==="
+if [ $NUMBER -le 10 ]; then
+    echo "The number $NUMBER is less than or equal to 10"
+else
+    echo "The number $NUMBER is greater than 10"
+fi
+
+# GREATER THAN OR EQUAL (-ge)
+echo ""
+echo "=== GREATER THAN OR EQUAL (-ge) ==="
+if [ $NUMBER -ge 10 ]; then
     echo "The number $NUMBER is greater than or equal to 10"
+else
+    echo "The number $NUMBER is less than 10"
+fi
+
+# EQUAL (-eq)
+echo ""
+echo "=== EQUAL (-eq) ==="
+if [ $NUMBER -eq 10 ]; then
+    echo "The number $NUMBER is equal to 10"
+else
+    echo "The number $NUMBER is NOT equal to 10"
+fi
+
+# NOT EQUAL (-ne)
+echo ""
+echo "=== NOT EQUAL (-ne) ==="
+if [ $NUMBER -ne 10 ]; then
+    echo "The number $NUMBER is NOT equal to 10"
+else
+    echo "The number $NUMBER is equal to 10"
 fi
---
+++ installation.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [$USERID -ne 0 ];then
+    echo "ERROR:: Please run this script with root privileges."
+    exit 1
+fi 
+
+#dnf list installed mysql
+#if [ $? -ne 0 ]; then
+#    dnf install mysql -y
+#    VALIDATE $? "mysql"
+#else
+#    echo -e "Mysql already installed.... $G Skipping the installation $N"
+#fi
+
+CHECK_ALREADY_INSTALLED $? "nginx"
+
+CHECK_ALREADY_INSTALLED(){
+    if [ $1 -ne 0 ]: then
+        echo -e "$Y $2 is not installed on the system. Proceeding with installation... $N"
+        dnf install nginx -y
+        VALIDATE $? "nginx"
+    else 
+    echo -e "$2 already installed.... $G Skipping the installation $N"
+    fi
+}
+
+VALIDATE(){
+     if [$1 -ne 0 ]; then
+    echo "ERROR:: Installinig $2 Failed"
+    exit 1
+else 
+    echo -e " $2  is Installed Successfully"
+fi
+}
