6c1152582e532c56e40e71e7d7126e2a2c741b11
sateesh thavitireddy <sateesh.thavitireddy@gmail.com>
2025-12-13 20:56:56 +0000

    Adding the scripts

--- logs.sh
+++ logs.sh
@@ -6,11 +6,38 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-if [ $USERID -ne 0 ]; then
-    echo "$R you cannnot run this script without root user $N"
-    exit 1
-fi
+
 
 LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
-echo -e "$G ${SCRIPT_NAME} $N"
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir $LOGS_FOLDER
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]; then
+    echo "$R you cannnot run this script without root user $N" | tee -a $LOG_FILE
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "INSTALLATION $2 ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "INSTALLATION $2 ... $G SUCCESS $N" | tee -a $LOG_FILE
+    fi 
+}
+
+CHECK_ALREADY_INSTALLED(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$G preparing to install the $2 $N" | tee -a $LOG_FILE
+        dnf install $2 -y &>>$LOG_FILE
+        VALIDATE $? $2
+    else
+      echo -e "$Y $2 is already installed on this server $N" | tee -a $LOG_FILE
+      echo -e "$Y $2 already installed on this server $N" | tee -a $LOG_FILE
+    fi
+}
+
+CHECK_ALREADY_INSTALLED $? "mysql" &>>$LOG_FILE
