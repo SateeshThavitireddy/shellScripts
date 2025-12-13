a37750eb1fd3e0659519f82b85882c3daca76d27
sateesh thavitireddy <sateesh.thavitireddy@gmail.com>
2025-12-13 14:09:59 +0000

    Adding the scripts

--- installation.sh
+++ installation.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-USERID=$(id)
+USERID=$(id -u)
 
-if [ $USERID -ne 0 ]; then
-    echo "ERROR:: Please run this script as root user"
+if [ $(USERID) -ne 0 ]; then
+    echo "you must run this scipt as root user"
     exit 1
 fi
 
 dnf install mysql -y
 
 if [ $? -ne 0 ]; then
-    echo "ERROR:: Installing MYSQL is failure"
+    echo "mysql installation failed"
     exit 1
-else
-    echo "INstalling MYSQL is SUCCESS"
-fi
+else 
+    echo "mysql installed successfully"
+fi  
