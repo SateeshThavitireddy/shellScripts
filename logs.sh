7ef29151e0db131a2dbed18f85bf39d761351972
sateesh thavitireddy <sateesh.thavitireddy@gmail.com>
2025-12-13 20:30:17 +0000

    Adding the scripts

---
+++ logs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ $USERID -ne 0 ]; then
+    echo "$R you cannnot run this script without root user $N"
+    exit 1
+fi
+
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
