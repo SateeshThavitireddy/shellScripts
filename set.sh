d97739e8f113f66ab681f408ed49bd08ac9ab821
sateesh thavitireddy <sateesh.thavitireddy@gmail.com>
2025-12-13 22:22:15 +0000

    Adding the scripts

--- set.sh
+++ set.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-trap 'echo "There is an error in $LINENUMBER, Command is: $BASH_COMMAND"'ERR
+trap 'echo "There is an error in $LINENO, Command is: $BASH_COMMAND"'ERR
 
 echo "Hello, this is set.sh script!"
 echo "Before Error"
