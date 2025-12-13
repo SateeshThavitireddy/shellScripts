4286c0385c24b9e0493f82b844a99428d2b3342c
sateesh thavitireddy <sateesh.thavitireddy@gmail.com>
2025-12-13 12:41:56 +0000

    Adding the scripts

--- dataTypes.sh
+++ dataTypes.sh
@@ -7,7 +7,7 @@ SUM=$(($NUMBER1 + $NUMBER2))
 
 echo "Sum is ${SUM}"
 
-LEADER=("MODI","PUTIN","TRUDO","TRUMP")
+LEADERS=("MODI","PUTIN","TRUDO","TRUMP")
 
 echo "ALL leaders: ${LEADERS[@]}"
 echo "First leader: ${LEADERS[0]}"
