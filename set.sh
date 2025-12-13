905ac6c72d34c0697959e13d979d261ded3d63c1
sateesh thavitireddy <sateesh.thavitireddy@gmail.com>
2025-12-13 22:21:17 +0000

    Adding the scripts

--- set.sh
+++ set.sh
@@ -7,4 +7,5 @@ trap 'echo "There is an error in $LINENUMBER, Command is: $BASH_COMMAND"'ERR
 echo "Hello, this is set.sh script!"
 echo "Before Error"
 
-ccaffss; dnfecho "After Error"
+ccaffss; dnf
+echo "After Error"
