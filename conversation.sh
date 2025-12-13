06c04dbeef527760976433046d73704a06fafa85
sateesh thavitireddy <sateesh.thavitireddy@gmail.com>
2025-12-13 11:46:55 +0000

    Adding the scripts

---
+++ conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Ramesh:; Hi Suresh, how are you?"
+echo "Suresh:; I'm good, Ramesh! How about you?"
+echo "Ramesh:; I'm doing well, thanks for asking."
+echo "Suresh:; Have you finished the project we were working on?"
+echo "Ramesh:; Yes, I completed it yesterday."
