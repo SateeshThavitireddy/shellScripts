4e2756beea003051c6ecd2fd6a38bd0293dc2da6
sateesh thavitireddy <sateesh.thavitireddy@gmail.com>
2025-12-13 14:45:40 +0000

    Adding the scripts

---
+++ ColorsFunctions.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+USERID = $(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R you cannot run this script as non root user $N"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "INSTALLATION FAILED FOR $2 $N"
+        exit 1
+    else 
+        echo -e "$G INSTALLATION SUCCESSFUL FOR $2 $N"
+    fi
+}
+
+CHECK_ALREADY_INSTALLED(){
+    if [ $1 -eq 0 ]; then
+        echo -e "$Y $2 already installed on this server $N"
+    else
+        echo "$G preparing to install $2 $N"
+        dnf install $2 -y
+        VALIDATE $? $2
+    fi
+}
+
+CHECK_ALREADY_INSTALLED $? "MYSQL"
