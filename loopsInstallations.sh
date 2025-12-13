7219e2f8e59b6d61eb37fa42585616996f8692bd
sateesh thavitireddy <sateesh.thavitireddy@gmail.com>
2025-12-13 21:41:31 +0000

    Adding the scripts

--- loopsInstallations.sh
+++ loopsInstallations.sh
@@ -31,7 +31,7 @@ VALIDATE(){
 for package in $@
 do 
     dnf list installed $package &>>$LOG_FILE
-    if[ $? -ne 0 ]; then
+    if [ $? -ne 0 ]; then
         dnf install $package -y &>>$LOG_FILE
         VALIDATE $? $package
     else
