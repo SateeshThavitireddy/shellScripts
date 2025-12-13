5e9093f192eb7a9a64280f3198f54fb265b759ad
sateesh thavitireddy <sateesh.thavitireddy@gmail.com>
2025-12-13 21:42:38 +0000

    Adding the scripts

--- loopsInstallations.sh
+++ loopsInstallations.sh
@@ -8,7 +8,7 @@ N="\e[0N"
 
 LOGS_FOLDER="var/log/shell-script"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
-LOG_FILE="$LOGS_FOLDER/SCRIPT_NAME.log"
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
 mkdir -p $LOGS_FOLDER
 echo "Script Started Execution at $(date +%s)" | tee -a $LOG_FILE
